dd05cbb665f1026e499dfe28e10f94fb11994b83
nikhildevassia <nikhildevasia2000@gmail.com>
2022-12-11 13:52:51 +0530

    capture video
     capture video and detect hand using cvzon.HandTrackingModule, then create another window for detected hand

--- datacollection.py
+++ datacollection.py
@@ -1,12 +1,26 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import numpy as np
 
 cap = cv2.VideoCapture(0)
-
+detector = HandDetector(maxHands=1)
+offset = 20
 
 while True:
     success, img = cap.read()
+    hands, img = detector.findHands(img)
+    try:
+        if hands:
+            hand = hands[0]
+            # getting bounding box information
+            x, y, w, h = hand['bbox']
+            # crop image -- starting [height: ending height][y:y+1], starting [width:ending width][x:x+1]
+            imgCrop = img[y - offset:y + offset + h, x - offset:x + offset + w]  # this will give the exact bounding box
+            cv2.imshow('ImageCrop', imgCrop)
+    except:
+        pass
     cv2.imshow('Image', img)
-    cv2.waitKey(1)
-
+    k = cv2.waitKey(1)
+    if k == 27:
+        cv2.destroyWindow()
 
